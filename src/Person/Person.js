29ef1729d021bb08e4256e409ef3652b06f35d3f
Matt Robinson <mthw.wm.robinson@gmail.com>
2018-04-10 22:10:48 -0400

    styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import './Person.css';
+
 const person = (props) => {
 
   return (
-    <div>
-      <h1>My name is {props.name} and I am { props.age} years old!</h1>
-      <p>{props.children}</p>
+    <div className="Person">
+      <h1 onClick={props.click}>My name is {props.name} and I am { props.age} years old!</h1>
+      <p onClick={props.click}>{props.children}</p>
+      <input type='text' onChange={props.changed} value={props.name}/>
     </div>
 
   )
